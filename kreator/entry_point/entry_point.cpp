185629e445843aba33c719f98ddd01c3b75ef216
Ashish <22274092+ashish1009@users.noreply.github.com>
2023-03-27 12:02:13 +0530

    Creating 'Kreator' Project
    1. Entry point defined for project

---
+++ kreator/entry_point/entry_point.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  kreator
+//
+//  Created by Ashish . on 27/03/23.
+//
+
+#include <iostream>
+
+int main() {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
