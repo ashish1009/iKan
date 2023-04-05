e89cefbc2cc1900bb61ffdd69dd01635ea53c2e6
Ashish <22274092+ashish1009@users.noreply.github.com>
2023-04-05 17:47:43 +0530

    Revert "Creating Core entry point"
    This reverts commit d2204c56a73d2dd986a1e20afcc8985cdab96166.

--- kreator/entry_point/entry_point.cpp
+++ kreator/entry_point/entry_point.cpp
@@ -5,4 +5,30 @@
 //  Created by Ashish . on 27/03/23.
 //
 
-#include <core_entry_point.h>
+/// This Function should be defined in client side for creating client application derived from base ikan::Application
+/// - Returns: Shared pointer of created application
+extern std::unique_ptr<ikan::Application> CreateApplication();
+
+int main() {
+  // Initialize the ikan Logger
+  auto core_level   = ikan::Logger::Level::Trace;
+  auto client_level = ikan::Logger::Level::Trace;
+  
+  ikan::Logger::Init(core_level, client_level, "../../../log/kreator.log");
+  
+#ifdef IK_DEBUG_FEATURE
+  auto spd_core_log_level = ikan::Logger::GetSpdLevelFromIKanLevel(core_level);
+  auto spd_client_log_level = ikan::Logger::GetSpdLevelFromIKanLevel(client_level);
+  
+  IK_TRACE("Core Entry Point", "Initialized the spd logger ");
+  IK_TRACE("Core Entry Point", "  Core   | {0}", ikan::Logger::GetLogLevelStringFromSpdLevel(spd_core_log_level));
+  IK_TRACE("Core Entry Point", "  Client | {0}", ikan::Logger::GetLogLevelStringFromSpdLevel(spd_client_log_level));
+#endif
+  
+  auto app = CreateApplication();
+  if (app) {
+    app->Run();
+  }
+  
+  return 0;
+}
