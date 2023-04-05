b0c570c4e8007e1bd9a7a897fc1c4defea7158a8
Ashish <22274092+ashish1009@users.noreply.github.com>
2023-04-05 18:50:32 +0530

    Creating Empty Batch Renderer Class

---
+++ ikan/implementation/renderer/utils/batch_2d_renderer.cpp
@@ -0,0 +1,12 @@
+//
+//  batch_2d_renderer.cpp
+//  ikan
+//
+//  Created by Ashish . on 05/04/23.
+//
+
+#include "batch_2d_renderer.hpp"
+
+namespace ikan {
+  
+} // namespace ikan
---
+++ ikan/interface/renderer/utils/batch_2d_renderer.hpp
@@ -0,0 +1,18 @@
+//
+//  batch_2d_renderer.hpp
+//  ikan
+//
+//  Created by Ashish . on 05/04/23.
+//
+
+#pragma once
+
+#include "core/math/aabb.hpp"
+
+namespace ikan {
+  
+  class BatchRenderer {
+  public:
+  };
+  
+} // namespace ikan
