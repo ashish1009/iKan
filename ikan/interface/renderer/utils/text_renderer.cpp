c4ec5ed16a3e2af2880fa511b6b1982ad866ca3d
Ashish <22274092+ashish1009@users.noreply.github.com>
2023-04-05 20:24:40 +0530

    Creating Empty Text Renderer class

---
+++ ikan/interface/renderer/utils/text_renderer.cpp
@@ -0,0 +1,12 @@
+//
+//  text_renderer.cpp
+//  ikan
+//
+//  Created by Ashish . on 05/04/23.
+//
+
+#include "text_renderer.hpp"
+
+namespace ikan {
+  
+} // namespace ikan
---
+++ ikan/interface/renderer/utils/text_renderer.hpp
@@ -0,0 +1,16 @@
+//
+//  text_renderer.hpp
+//  ikan
+//
+//  Created by Ashish . on 05/04/23.
+//
+
+#pragma once
+
+namespace ikan {
+  
+  class TextRenderer {
+  public:
+  };
+  
+} // namespace ikan
