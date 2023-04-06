392358ab722bee58499f97197da25726f70c43a2
Ashish <22274092+ashish1009@users.noreply.github.com>
2023-04-06 11:15:03 +0530

    Rendering Renderer Stats widget with Horizontal scrolling

--- ikan/implementation/renderer/utils/renderer_stats.cpp
+++ ikan/implementation/renderer/utils/renderer_stats.cpp
@@ -15,19 +15,98 @@ namespace ikan {
     CHECK_WIDGET_FLAG(is_open);
     
     ImGui::Begin("Renderer 2D Stats", is_open);
-    ImGui::PushID("Renderer 2D Stats");
+    ImGui::SetNextWindowContentSize(ImVec2(480, 0.0f));
+    ImGui::BeginChild("##RendererStats2D", ImVec2(0, ImGui::GetFontSize() * 2), false, ImGuiWindowFlags_HorizontalScrollbar);
 
-    ImGui::PopID();
+    ImGui::Columns(6);
+    
+    ImGui::SetColumnWidth(0, 80);
+    ImGui::Text("%d", max_quads);
+    PropertyGrid::HoveredMsg("Max Quads");
+    ImGui::NextColumn();
+    
+    ImGui::SetColumnWidth(1, 80);
+    ImGui::Text("%d", max_circles);
+    PropertyGrid::HoveredMsg("Max Circles");
+    ImGui::NextColumn();
+    
+    ImGui::SetColumnWidth(2, 80);
+    ImGui::Text("%d", max_lines);
+    PropertyGrid::HoveredMsg("Max Lines");
+    ImGui::NextColumn();
+    
+    ImGui::SetColumnWidth(3, 80);
+    ImGui::Text("%d", quads);
+    PropertyGrid::HoveredMsg("Num Quads Rendered");
+    ImGui::NextColumn();
+    
+    ImGui::SetColumnWidth(4, 80);
+    ImGui::Text("%d", circles);
+    PropertyGrid::HoveredMsg("Num Circles Rendered");
+    ImGui::NextColumn();
+    
+    ImGui::SetColumnWidth(5, 80);
+    ImGui::Text("%d", lines);
+    PropertyGrid::HoveredMsg("Num Lines Rendered");
+    ImGui::NextColumn();
+
+    ImGui::Columns(1);
+    ImGui::EndChild();
     ImGui::End();
   }
   
   void RendererStatistics::RenderGui(bool* is_open) {
     CHECK_WIDGET_FLAG(is_open);
 
-    ImGui::Begin("Renderer Stats", is_open);
-    ImGui::PushID("Renderer Stats");
+    static std::string hovered_message = "";
 
-    ImGui::PopID();
+    ImGui::Begin("Renderer Stats", is_open);
+    ImGui::SetNextWindowContentSize(ImVec2(620.0f, 0.0f));
+    ImGui::BeginChild("##RendererStats", ImVec2(0, ImGui::GetFontSize() * 2), false, ImGuiWindowFlags_HorizontalScrollbar);
+    ImGui::Columns(6);
+
+    ImGui::SetColumnWidth(0, 60);
+    ImGui::Text("%d", draw_calls);
+    PropertyGrid::HoveredMsg("Draw Calls");
+    ImGui::NextColumn();
+
+    ImGui::SetColumnWidth(1, 100);
+    ImGui::Text("%d", vertex_count);
+    PropertyGrid::HoveredMsg("Vertex Counts");
+    ImGui::NextColumn();
+
+    ImGui::SetColumnWidth(2, 100);
+    ImGui::Text("%d", index_count);
+    PropertyGrid::HoveredMsg("Index Counts");
+    ImGui::NextColumn();
+
+    ImGui::SetColumnWidth(3, 120);
+    ImGui::Text("%.3f KB", (float)vertex_buffer_size / (float)1000.0f);
+    hovered_message  = "Vertex Buffer Size (Not counting buffer used to render Text) \n";
+    hovered_message += "MB    : " + std::to_string((float)vertex_buffer_size / (float)1000000.0f) + "\n";
+    hovered_message += "Bytes : " + std::to_string(vertex_buffer_size);
+    PropertyGrid::HoveredMsg(hovered_message.c_str());
+    ImGui::NextColumn();
+
+    ImGui::SetColumnWidth(4, 120);
+    ImGui::Text("%.3f KB", (float)index_buffer_size / (float)1000.0f);
+    hovered_message  = "Index Buffer Size \n";
+    hovered_message += "MB    : " + std::to_string((float)index_buffer_size / (float)1000000.0f) + "\n";
+    hovered_message += "Bytes : " + std::to_string(index_buffer_size);
+    PropertyGrid::HoveredMsg(hovered_message.c_str());
+    ImGui::NextColumn();
+
+    ImGui::SetColumnWidth(5, 120);
+    ImGui::Text("%.3f KB", (float)texture_buffer_size / (float)1000.0f);
+    hovered_message  = "Texure Data Size \n";
+    hovered_message += "MB    : " + std::to_string((float)texture_buffer_size / (float)1000000.0f) + "\n";
+    hovered_message += "Bytes : " + std::to_string(texture_buffer_size);
+    PropertyGrid::HoveredMsg(hovered_message.c_str());
+
+    ImGui::NextColumn();
+    
+    ImGui::Columns(1);
+    ImGui::EndChild();
     ImGui::End();
   }
   
