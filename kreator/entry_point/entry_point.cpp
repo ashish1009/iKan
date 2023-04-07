b323144b9f0097cc5513525551fe9c808e997639
Ashish <22274092+ashish1009@users.noreply.github.com>
2023-04-07 18:22:55 +0530

    Disable Extra log Module

--- kreator/entry_point/entry_point.cpp
+++ kreator/entry_point/entry_point.cpp
@@ -15,6 +15,8 @@ class KreatorApp : public ikan::Application {
 public:
   KreatorApp(const ikan::Application::Specification& application_spec, kreator::GameType game_type):
   ikan::Application(application_spec) {
+    ikan::Logger::DisableModule(ikan::LogModule::CharTexture);
+    
     IK_TRACE("Kreator Application", "Creating Kreator Application Instance ...");
     PushLayer(std::make_shared<kreator::RendererLayer>(game_type));
   }
@@ -36,13 +38,9 @@ std::unique_ptr<ikan::Application> CreateApplication() {
   application_spec.os = ikan::OperatingSystem::Mac;
   
   // Utils
-  application_spec.name = "IKan";
-  application_spec.workspace_path = "../../../";
-  application_spec.client_asset_path = "../../../kreator/editor/assets/";
-  application_spec.save_ini_file_path = "../../../kreator/editor/ini/editor.ini";
   
   // Window Specification
-  application_spec.window_specification.title = "Kreator";
+  application_spec.window_specification.title = "Untitled";
   application_spec.window_specification.width = 2100;
   application_spec.window_specification.height = 900;
   application_spec.window_specification.v_sync = true;
@@ -52,6 +50,21 @@ std::unique_ptr<ikan::Application> CreateApplication() {
   application_spec.resizable = true;
   application_spec.start_maximized = false;
   
+  // Create the instance of applciaiton based on the type of suppored applucaiton
+  switch (game_type) {
+    case kreator::GameType::Editor :
+      application_spec.name = "IKan";
+      application_spec.workspace_path = "../../../";
+      application_spec.client_asset_path = "../../../kreator/editor/assets/";
+      application_spec.save_ini_file_path = "../../../kreator/editor/ini/editor.ini";
+
+      application_spec.window_specification.title = "Kreator";
+      break;
+      
+    default:
+      break;
+  };
+
   return std::make_unique<KreatorApp>(application_spec, game_type);
 }
 
