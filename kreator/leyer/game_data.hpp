8f028ea23fde85b6a563199f7cd690f57b231fdf
Ashish <22274092+ashish1009@users.noreply.github.com>
2023-04-13 15:16:56 +0530

    Fixing Scene path in editor

--- kreator/leyer/game_data.hpp
+++ kreator/leyer/game_data.hpp
@@ -54,12 +54,12 @@ namespace kreator {
 
     std::string GameName() const override { return "Kreator Editor"; }
     glm::vec4 GetBgColor() const override { return {0.5f, 0.2f, 0.2f, 1.0f}; }
-    std::string GetScenePath() const override { return DM::ClientAsset("scenes/"); }
+    std::string GetScenePath() const override { return DM::WorkspacePath("/kreator/editor/scenes"); }
     std::string SavedScene() const override { return ""; };
     std::string CbpRootDir() const override { return GetScenePath(); };
     std::vector<std::filesystem::path> FavDirecotries() const override {
       return {
-        DM::ClientAsset("scenes"),
+        GetScenePath(),
         DM::ClientAsset("textures"),
       };
     };
