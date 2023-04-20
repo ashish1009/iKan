dbbfe930e1d5fc03029a18bda793a1bd2aeaa4ed
Ashish <22274092+ashish1009@users.noreply.github.com>
2023-04-20 18:52:21 +0530

    Creating Fixed camera class

---
+++ ikan/interface/camera/fixed_camera.hpp
@@ -0,0 +1,37 @@
+//
+//  fixed_camera.hpp
+//  ikan
+//
+//  Created by Ashish . on 20/04/23.
+//
+
+#pragma once
+
+#include "camera/camera.hpp"
+
+namespace ikan {
+  
+  class FixedCamera : public Camera {
+  public:
+    /// This constructor creates the Scene Camera instance
+    FixedCamera();
+    /// Default Scene Camera Desctructorr
+    virtual ~FixedCamera();
+
+    /// This function updates the Viewport size of camera
+    /// - Parameters:
+    ///   - width: new width
+    ///   - height: new height
+    void SetViewportSize(uint32_t width, uint32_t height);
+
+    /// This function renders Imgui pannel for Scene should cal this function between Imgui::Begin and Imgui::End
+    void RenderGui();
+
+  private:
+    /// This function recalculate the projection matrix
+    void RecalculateProjection();
+
+    float orthographic_size_ = 10.0f;
+  };
+  
+} // namespace ikan
--- ikan/interface/core/debug/logger.hpp
+++ ikan/interface/core/debug/logger.hpp
@@ -18,7 +18,7 @@ namespace ikan {
   f(Application) f(Window) f(DirectoryManager) f(LayerStack) \
   f(Renderer) f(GraphicsContext) f(Batch2DRenderer) f(TextRenderer) \
   f(Pipeline) f(VertexBuffer) f(IndexBuffer) f(Shader) f(Texture) f(FrameBuffer) f(CharTexture) f(SubTexture) \
-  f(EditorCamera) f(SceneCamera) \
+  f(EditorCamera) f(SceneCamera) f(FixedCamera) \
   f(ContentBrowserPanel) \
 f(Scene) f(ScenePanelManager) f(SceneSerializer) f(Entity) f(Component) \
   f(Imgui) \
