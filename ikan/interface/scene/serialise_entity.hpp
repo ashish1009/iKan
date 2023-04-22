22203d80c0c107e1fc540746082351b1cfc207cf
Ashish <22274092+ashish1009@users.noreply.github.com>
2023-04-22 16:46:41 +0530

    Adding Comments in Entity Serialiser

--- ikan/interface/scene/serialise_entity.hpp
+++ ikan/interface/scene/serialise_entity.hpp
@@ -14,11 +14,40 @@ namespace ikan {
   
   class EntitySerialiser {
   public:
+    /// This function serialise the box 2d Collider component
+    /// - Parameters:
+    ///   - out: YAML out reference
+    ///   - bcc: box collider component
+    ///   - identifier: string identifier
     static void SerializeBoxCollider(YAML::Emitter& out, const Box2DColliderComponent& bcc, std::string identifier);
+    /// This function deserialise the box 2d Collider component
+    /// - Parameters:
+    ///   - bcc: box collider component
+    ///   - box_colloider_component: box collider data from YAML
+    ///   - identifier: string identifier
     static void DeserializeBoxCollider(Box2DColliderComponent& bcc, const YAML::Node& box_colloider_component, std::string identifier);
+    /// This function serialise the circle Collider component
+    /// - Parameters:
+    ///   - out: yamml out reference
+    ///   - ccc: cirlce collider component
+    ///   - identifier: string identifier
     static void SerializeCircleCollider(YAML::Emitter& out, const CircleColliiderComponent& ccc, std::string identifier);
+    /// This function serialise the circle Collider component
+    /// - Parameters:
+    ///   - ccc: cirlce collider component
+    ///   - circle_colloider_component: cirlce collider data from YAML
+    ///   - identifier: string identifier
     static void DeserializeCircleCollider(CircleColliiderComponent& ccc, const YAML::Node& circle_colloider_component, std::string identifier);
+    /// This function serialise entity with all component
+    /// - Parameters:
+    ///   - out: out Refenrence of YAML
+    ///   - entity: entity
     static void SerialiseEntity(YAML::Emitter& out, Entity entity);
+    /// This function deserialise the entity of all compoennt
+    /// - Parameters:
+    ///   - entity: entity data
+    ///   - e: entity
+    ///   - scene_: scene
     static void DeserislizeEntity(YAML::Node entity, Entity e, Scene* scene_);
     
     MAKE_PURE_STATIC(EntitySerialiser);
