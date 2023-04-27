ef43e2c57c6243bb2ab05f8ef43eb44d89fb6f5b
Ashish <22274092+ashish1009@users.noreply.github.com>
2023-04-27 18:41:07 +0530

    Adding API to Spawn the run time Item in Mario

--- kreator/games/mario/src/runtime_items.cpp
+++ kreator/games/mario/src/runtime_items.cpp
@@ -10,22 +10,10 @@
 
 namespace mario {
   
-  struct ItemData {
-    std::string name;
-    std::string scrip_name;
-    ScriptLoaderFn loader_fun;
-    
-    ItemData() = default;
-    ItemData(const std::string& name, const std::string& scrip_name, ScriptLoaderFn fun) : name(name), scrip_name(scrip_name), loader_fun(fun) { }
-  };
+  std::shared_ptr<RuntimeItemData> RuntimeItem::data_;
   
-  struct RuntimeItemData {
-    std::unordered_map<Items, ItemData> item_map;
-  };
-  static std::shared_ptr<RuntimeItemData> data;
-
   void RuntimeItem::Init() {
-    data = std::make_shared<RuntimeItemData>();
+    data_ = std::make_shared<RuntimeItemData>();
     
     static auto coin_script_loader = ScriptLoader(mario::CoinController);
     static auto mushroom_script_loader = ScriptLoader(mario::MushroomController);
@@ -34,19 +22,19 @@ namespace mario {
     static auto star_script_loader = ScriptLoader(mario::FireballController);
     static auto score_script_loader = ScriptLoader(mario::ScoreController);
 
-    data->item_map[Items::Coin] = { "Block Coin", "mario::CoinController", coin_script_loader };
-    data->item_map[Items::Mushroom] = { "Mushroom", "mario::MushroomController", mushroom_script_loader };
-    data->item_map[Items::Flower] = { "Flower", "mario::FlowerController", flower_script_loader };
-    data->item_map[Items::Fireball] = { "Fireball", "mario::FireballController", fireball_script_loader };
-    data->item_map[Items::Star] = { "Star", "mario::StarController", star_script_loader };
-    data->item_map[Items::Score] = { "Score", "mario::ScoreController", score_script_loader };
+    data_->item_map[Items::Coin] = { "Block Coin", "mario::CoinController", coin_script_loader };
+    data_->item_map[Items::Mushroom] = { "Mushroom", "mario::MushroomController", mushroom_script_loader };
+    data_->item_map[Items::Flower] = { "Flower", "mario::FlowerController", flower_script_loader };
+    data_->item_map[Items::Fireball] = { "Fireball", "mario::FireballController", fireball_script_loader };
+    data_->item_map[Items::Star] = { "Star", "mario::StarController", star_script_loader };
+    data_->item_map[Items::Score] = { "Score", "mario::ScoreController", score_script_loader };
     
     MARIO_LOG("Initialised the Runtime Items Manager");
   }
   
   void RuntimeItem::Shutdown() {
-    data.reset();
+    data_.reset();
     MARIO_LOG("Destroyed the Runtime Items Manager");
   }
-  
+
 } // namespace mario
--- kreator/games/mario/src/runtime_items.hpp
+++ kreator/games/mario/src/runtime_items.hpp
@@ -7,6 +7,8 @@
 
 #pragma once
 
+#include "sprite_manager.hpp"
+
 namespace mario {
   
   using namespace ikan;
@@ -33,12 +35,53 @@ namespace mario {
   class ScoreController : public ScriptableEntity {
   };
   
+  struct ItemData {
+    std::string name;
+    std::string scrip_name;
+    ScriptLoaderFn loader_fun;
+    
+    ItemData() = default;
+    ItemData(const std::string& name, const std::string& scrip_name, ScriptLoaderFn fun) : name(name), scrip_name(scrip_name), loader_fun(fun) { }
+  };
+  
+  struct RuntimeItemData {
+    std::unordered_map<Items, ItemData> item_map;
+  };
+  
   class RuntimeItem {
   public:
     /// This function initialise the runtime item scripts and data
     static void Init();
     /// This function sutdowns the runtime item scripts and data
     static void Shutdown();
+    
+    template <typename... Args>
+    /// This function Spawns the Runtime Item by creating Entity and adding importatn Compoennts
+    /// - Parameters:
+    ///   - item: Item type
+    ///   - scene: Scene pointer
+    ///   - pos: Spawn Position
+    ///   - args: Arguments required for Item Scrips....
+    static void Spawn(Items item, Scene* scene, const glm::vec2& pos, Args&&... args) {
+      static std::shared_ptr<Texture> items = SpriteManager::GetTexture(SpriteType::Items);
+
+      // Create Runtime Entity
+      auto run_time_entity = scene->CreateEntity(data_->item_map.at(item).name);
+      
+      // Update the Spawn Position
+      auto& tc = run_time_entity.GetComponent<TransformComponent>();
+      tc.UpdatePosition(glm::vec3(pos, 0.1f));
+
+      if (item == Items::Score) {
+        // Text
+      }
+      else {
+        // Quad
+      }
+    }
+    
+  private:
+    static std::shared_ptr<RuntimeItemData> data_;
   };
   
 } // namespace mario
